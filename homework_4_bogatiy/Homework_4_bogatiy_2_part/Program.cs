40bcd85b954b7ea3c17041606823958e25805018
Анатолий Богатый <nikitasotnikov0@gmail.com>
2020-12-17 00:30:44 +0300

    DZ_4_completed
    Готово

---
+++ homework_4_bogatiy/Homework_4_bogatiy_2_part/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace DZ4
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите размеры матрицы: ");
+            int rows = int.Parse(Console.ReadLine());
+            int cols = int.Parse(Console.ReadLine());
+            while (rows < 0 || cols < 0)
+            {
+                Console.WriteLine("Неверно введены размеры матрицы, повторите ввод!");
+                rows = int.Parse(Console.ReadLine());
+                cols = int.Parse(Console.ReadLine());
+            }
+            int[,] array = new int[rows, cols];
+            int counterForCol = InitArray(ref array);
+            Console.WriteLine($"Количество столбцов, не содержащих ни одного нулевого элемента = {counterForCol}");
+            Console.WriteLine("-------Матрица--------");
+            PrintArray(ref array);
+            Sort(ref array);
+            Console.WriteLine("-------Отсортированный Массив--------");
+            PrintArray(ref array);
+        }
+
+        /// Забивает массив вещественными числами и возвращает количество столбцов, не содержащих ни одного нулевого элемента
+        static int InitArray(ref int[,] array)
+        {
+            int counter = 0;
+            bool indicator = true;
+            var r = new Random();
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                indicator = true;
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = r.Next(-10, 11);
+                    if (!indicator && array[i, j] == 0)
+                        indicator = false;
+                }
+                if (indicator) counter++;
+            }
+            return counter;
+        }
+
+        /// Выводит матрицу на консоль
+        static void PrintArray(ref int[,] array)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write($"{array[i, j],4}");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        /// Сортировка матрицы
+        static void Sort(ref int[,] array)
+        {
+            int[] arOfCharacter = new int[array.GetLength(0)];
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                arOfCharacter[i] = 0;
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    if (array[i, j] % 2 == 0 && array[i, j] > 0)
+                        arOfCharacter[i]++;
+                }
+            }
+            for (int k = 0; k < array.GetLength(0); k++)
+            {
+                for (int i = 0; i < array.GetLength(0) - 1; i++)
+                {
+                    if (arOfCharacter[i] > arOfCharacter[i + 1])
+                    {
+                        int temp = 0;
+                        for (int j = 0; j < array.GetLength(1); j++)
+                        {
+                            temp = array[i, j];
+                            array[i, j] = array[i + 1, j];
+                            array[i + 1, j] = temp;
+                        }
+                        temp = arOfCharacter[i];
+                        arOfCharacter[i] = arOfCharacter[i + 1];
+                        arOfCharacter[i + 1] = temp;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ homework_4_bogatiy/homework_4_bogatiy/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+
+namespace DZ4
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите размер массива: ");
+            int n = int.Parse(Console.ReadLine());
+            while (n < 0)
+            {
+                Console.WriteLine("Неверно введен размер массива, повторите ввод!");
+                n = int.Parse(Console.ReadLine());
+            }
+            double[] array = new double[n];
+            double SumOfPositiveElsem = InitArray(ref array);
+            Console.WriteLine($"Сумма положительных элементов массива = {SumOfPositiveElsem:##.###}");
+            double MultiplicationOfElem = Multiplication(ref array);
+            Console.WriteLine($"Произведение элементов между максимальным и минимальным элементами массива = {MultiplicationOfElem: ##.###}.\n" +
+                $"Если равно 1, то элементов между максимальным и минимальным элементами массива не существует.");
+            Console.WriteLine("-------Массив--------");
+            PrintArray(ref array);
+            Sort(ref array);
+            Console.WriteLine("-------Отсортированный Массив--------");
+            PrintArray(ref array);
+
+
+        }
+
+        /// Забивает массив вещественными числами и возвращает сумму положительных элементов
+        static double InitArray(ref double[] array)
+        {
+            int i = 0;
+            var r = new Random();
+            double SumOfPositiveElsem = 0;
+            while (i != array.Length)
+            {
+                array[i] = r.NextDouble() * Math.Pow(-1, (r.Next(1, 3)));
+                if (array[i] > 0)
+                    SumOfPositiveElsem += array[i];
+                i++;
+            }
+            return SumOfPositiveElsem;
+        }
+
+        /// Выводит массив на консоль
+        static void PrintArray(ref double[] array)
+        {
+            int i = 0;
+            while (i != array.Length)
+            {
+                Console.WriteLine(Math.Round(array[i++], 2) + "  ");
+            }
+        }
+
+        /// Возвращает произведение элементов между максимальным и минимальным элементами массива
+        static double Multiplication(ref double[] array)
+        {
+            double Multiplication = 1;
+            double max = 0;
+            double min = 1;
+            int IndexOfMax = 0;
+            int IndexOfMin = array.Length - 1;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] > max)
+                {
+                    max = array[i];
+                    IndexOfMax = i;
+                }
+                if (array[i] < min)
+                {
+                    min = array[i];
+                    IndexOfMin = i;
+                }
+            }
+            if (IndexOfMax < IndexOfMin)
+            {
+                int temp = IndexOfMax;
+                IndexOfMax = IndexOfMin;
+                IndexOfMin = temp;
+            }
+            for (int i = IndexOfMin; i < IndexOfMax; i++)
+                Multiplication *= array[i];
+            return Multiplication;
+        }
+
+        /// Упорядовачивание элементов по убыванию
+        static void Sort(ref double[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array.Length - 1; j++)
+                {
+                    if (array[j] < array[j + 1])
+                    {
+                        double temp = array[j + 1];
+                        array[j + 1] = array[j];
+                        array[j] = temp;
+                    }
+                }
+            }
+        }
+    }
+}
